ae89c2bdd5edf2f72d0434a7e034532d8d85f60b
Alok Mishra <mishralok794@gmail.com>
2023-01-15 02:33:38 +0530

    i uploaded data into the cloud firestore

--- src/App.js
+++ src/App.js
@@ -1,41 +1,56 @@
 
 import './App.css';
 import { db } from './firebase-config';
-import { getDocs, doc, collection, data, id } from 'firebase/firestore';
-import { useEffect } from 'react';
-// import { async } from '@firebase/util';
+import { getDocs, doc, collection, addDoc, } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  let [newUser,setUser]=useState("");
+  let [img,setImg]=useState(null);
 
   let dataRef = collection(db, "users");
   // console.log(dataRef)
 
-  function handleUser(data) {
-    console.log("hey user");
-    console.log(data)
-  }
+  
 
-  useEffect(() => {
-    async function handleInfo() {
-      let d = await getDocs(dataRef);
-      // console.log(d)
-      d.forEach((dc) => {
-        console.log(dc.id, "=>", dc.data());
+  // useEffect(() => {
+  //   async function handleInfo() {
+  //     let d = await getDocs(dataRef);
+  //     // console.log(d)
+  //     d.forEach((dc) => {
+  //       console.log(dc.id, "=>", dc.data());
+  //     })
+
+  //   }
+  //   console.log("QQQQQQ")
+  //   handleInfo()
+  // }, []);
+
+  console.log(img)
+   console.log(newUser);
+   
+  async function addData(){
+    try{
+      let user=await addDoc(dataRef,{
+        namw:newUser
       })
-
+      console.log(user);
+      console.log("data added sucessfully");
+    }catch(e){
+      console.log(e);
+      console.log("not added data")
     }
-    console.log("QQQQQQ")
-    handleInfo()
-  }, []);
+  }
 
 
   return (
     <div className="App">
       <div className='input'>
-
         <label>Wrtie something &nbsp;&nbsp;</label>
-        <input placeholder='write here' type='text' onChange={(e) => { handleUser(e.target.value) }}></input>
-        <button >add data</button>
+        <input placeholder='write here' type='text' onChange={(e) => { setUser(e.target.value)}}></input>
+        <br></br>
+        <button onClick={addData}>add data</button>
       </div>
     </div>
   );
